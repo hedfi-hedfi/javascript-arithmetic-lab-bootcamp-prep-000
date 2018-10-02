dfd086ef62a447a25ba6104084061db1364c23b9
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-02 20:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a, b) { return a + b }
 function subtract(a, b) { return a - b }
 function multiply(a, b) { return a * b }
 function divide(a, b) { return a / b }
-function in(n) {return ++n}
+function inc(n) {return ++n}
 function dec(n) {return --n}
 function makeInt(n) {return parseInt(n) }
 function preserveDecimal(n) { return parseFloat(n) }
